b5a7a736fe088da57ecb54bc2f4f160b38910f9e
GiangBeo91 <nguyentruonggiang91@gmail.com>
2014-06-12 22:34:15 +0700

    edit code

--- application/modules/home/views/faq_detail.php
+++ application/modules/home/views/faq_detail.php
@@ -3,4 +3,7 @@
  <?php echo $faq_detail[0]['question'];?>
  <strong>TRẢ LỜI</strong><br />
   <?php echo $faq_detail[0]['answer'];?>
+  <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo full_url_($_SERVER);?>;width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=771797639499496" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+                  <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<g:plusone></g:plusone>
  </div>
--- application/modules/home/views/sale_detail.php
+++ application/modules/home/views/sale_detail.php
@@ -1,5 +1,7 @@
  <div class="col-md-9 col-lg-9 col-sm-9">
  <h1><?php echo $detail_sale[0]['title'];?></h1>
  <?php echo $detail_sale[0]['content'];?>
- 
+ <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo full_url_($_SERVER);?>;width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=771797639499496" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+                  <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<g:plusone></g:plusone>
  </div>
